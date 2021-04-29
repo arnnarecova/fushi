7e2d57e40fce1e88db3632baec377e19b8b0deb2
arnnarecova <yiluwei54229@126.com>
2021-04-29 11:00:01 +0800

    线性查找

--- chazhao/chazhao/main.cpp
+++ chazhao/chazhao/main.cpp
@@ -8,3 +8,22 @@
 
 //找x
 #include <stdio.h>
+int main(){
+    int buf[200];
+    int n;
+    while (scanf("%d", &n) != EOF) {
+        for (int i = 0; i<n; i++) {
+            scanf("%d", &buf[i]);
+        }
+        int x, ans=-1;
+        scanf("%d", &x);
+        for (int i=0; i<n; i++) {
+            if (x == buf[i]) {
+                ans = i;
+                break;
+            }
+        }
+        printf("%d", ans);
+    }
+}
+
