16acd01fb7bf9c7df017310e4cce701da953e231
arnnarecova <yiluwei54229@126.com>
2021-05-01 21:04:25 +0800

    %运算符。。。这个有问题，看不懂PDF上写的

---
+++ math/math/main.cpp
@@ -0,0 +1,11 @@
+//
+//  main.cpp
+//  math
+//
+//  Created by 刘文栖 on 2018/3/5.
+//  Copyright © 2018年 刘文栖. All rights reserved.
+//
+
+// 取余问题  a % b 可以用公式 a - (a / b) * b 算出  eg:-15 % 2 == -15 - (-15 / 2) * 2 == -15 - (-7) * 2 == -1
+
+
