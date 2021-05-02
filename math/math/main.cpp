67606edb36d58df74bc8a94264ed0054860ac21e
arnnarecova <yiluwei54229@126.com>
2021-05-02 16:19:45 +0800

    最小公倍数

--- math/math/main.cpp
+++ math/math/main.cpp
@@ -68,39 +68,59 @@
 //}
 
 //数制转换
+//#include <stdio.h>
+//#include <string.h>
+//int main () {
+//    int a, b;
+//    char str[40];
+//    while (scanf("%d%s%d", &a, str, &b) != EOF) {
+//        int tmp = 0, lenth = strlen(str), c = 1;
+//        for (int i=lenth - 1; i >= 0; i--) {
+//            int x;
+//            if (str[i] >= '0' && str[i] <= '9') {
+//                x = str[i] - '0';
+//            } else if (str[i] >= 'a' && str[i] <= 'z') {
+//                x = str[i] - 'a' + 10;
+//            } else {
+//                x = str[i] - 'A' + 10;
+//            }
+//            tmp += x*c;
+//            c *= a;
+//        }
+//        char ans[40], size = 0;
+//        do {
+//            int x = tmp % b;
+//            ans[size++] = (x < 10) ? x + '0' : x - 10 + 'A';
+//            tmp /= b;
+//        } while(tmp);
+//        for (int i=size-1; i>=0; i--) {
+//            printf("%c", ans[i]);
+//        }
+//        printf("\n");
+//    }
+//}
+
+//最大公约数
 #include <stdio.h>
-#include <string.h>
-int main () {
+//int gcd(int a, int b){//递归版本
+//    if (b == 0) return a;
+//    else return gcd(b, a%b);
+//}
+int gcd(int a, int b) {//非递归版本
+    while (b != 0) {
+        int t = a % b;
+        a = b;
+        b = t;
+    }
+    return a;
+
+}
+int main(){
     int a, b;
-    char str[40];
-    while (scanf("%d%s%d", &a, str, &b) != EOF) {
-        int tmp = 0, lenth = strlen(str), c = 1;
-        for (int i=lenth - 1; i >= 0; i--) {
-            int x;
-            if (str[i] >= '0' && str[i] <= '9') {
-                x = str[i] - '0';
-            } else if (str[i] >= 'a' && str[i] <= 'z') {
-                x = str[i] - 'a' + 10;
-            } else {
-                x = str[i] - 'A' + 10;
-            }
-            tmp += x*c;
-            c *= a;
-        }	
-        char ans[40], size = 0;
-        do {
-            int x = tmp % b;
-            ans[size++] = (x < 10) ? x + '0' : x - 10 + 'A';
-            tmp /= b;
-        } while(tmp);
-        for (int i=size-1; i>=0; i--) {
-            printf("%c", ans[i]);
-        }
-        printf("\n");
+    while (scanf("%d%d", &a, &b) != EOF) {
+        printf("%d\n", gcd(a, b));
     }
 }
-
-
 
 
 
