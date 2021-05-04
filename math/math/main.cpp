6c681788c82d4a3b5cec27884141fc3d040d0fa8
arnnarecova <yiluwei54229@126.com>
2021-05-04 10:55:37 +0800

    还是看了 似乎没有那么难 有空多看两眼

--- math/math/main.cpp
+++ math/math/main.cpp
@@ -247,7 +247,52 @@
 //}
 
 //整除问题 p99页 回头看
-
+#include <stdio.h>
+#include <string.h>
+bool mark[1010];
+int prime[1010];
+int primeSize;
+void init(){
+    primeSize = 0;
+    for (int i=2; i<1000; i++) {
+        if (mark[i]) continue;
+        mark[i] = true;
+        prime[primeSize++] = i;
+        for (int j=i*i; j<=1000; j+=i) {
+            mark[j] = true;
+        }
+    }
+}
+int cnt[1010];
+int cnt2[1010];
+int main(){
+    int n, a;
+    init();
+    while (scanf("%d%d", &n, &a) != EOF) {
+        for (int i=0; i<primeSize; i++) {
+            cnt[i] = cnt[2] = 0;
+        }
+        for (int i=0; i<primeSize; i++) {
+            int t = n;
+            while (t) {
+                cnt[i] += t / prime[i];
+                t = t / prime[i];
+            }
+        }
+        int ans = 123123123;
+        for (int i=0; i<primeSize; i++) {
+            while (a % prime[i] == 0) {
+                cnt2[i]++;
+                a /= prime[i];
+            }
+            if (cnt2[i] == 0) continue;
+            if (cnt[i] / cnt2[i] < ans) {
+                ans = cnt[i] / cnt2[i];
+            }
+        }
+        printf("%d", ans);
+    }
+}
 
 
 
